4cada30bad5176e75c29fd384da3b1d1f23d47e9
DELL <DELL@DESKTOP-S0T5UCG>
2024-06-28 07:55:18 +0530

    Maven Second Project

---
+++ MavenSecondProject/src/main/java/MavenSecondPack/Encapsulation2.java
@@ -0,0 +1,13 @@
+package MavenSecondPack;
+
+public class Encapsulation2 {
+
+	public static void main(String[] args) {
+		Encapsulation1 bb = new Encapsulation1();
+		
+		bb.setDetails(101, "John");
+		bb.getDetails();
+
+	}
+
+}
